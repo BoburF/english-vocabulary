51cf210f6da595b771c1f07c5dfd379c1fb2e398
Bobur <changeitgo3@gmail.com>
2022-09-17 12:25:44 +0500

    feat: all pages

--- client/src/pages/User/Random/Random.jsx
+++ client/src/pages/User/Random/Random.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { useState } from 'react';
+import './Random.scss'
 
 const Random = () => {
+  const [random, setRandom] = useState('')
+
+  const Random = () => {
+    console.log('mana')
+    setRandom('mana')
+  }
+
   return (
-    <div>Random</div>
+    <div className='Random'>
+      <h2 className='title'>Random word your vocabulary</h2>
+      <div className="random_word">
+        <button title='Random word' onClick={()=>Random()}>Random</button>
+        <p>{random}</p>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/User/Settings/Settings.jsx
+++ client/src/pages/User/Settings/Settings.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { useState } from 'react';
+import './Settings.scss'
 
 const Settings = () => {
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
   return (
-    <div>Settings</div>
+    <div className='Settings'>
+      <h2 className='title'>User settings</h2>
+      <form className='form'>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+        <input type="text" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <button title='Change user data'>Change settings</button>
+      </form>
+    </div>
   )
 }
 
